7d0232217235328ac08f768d42911299002886b8
AndreasGottardi <andreas@goa.systems>
2022-09-30 17:17:55 +0200

    Initial commit

---
+++ src/main/java/goa/systems/eventman/Event.java
@@ -0,0 +1,34 @@
+package goa.systems.eventman;
+
+import java.util.GregorianCalendar;
+
+public class Event {
+
+	private Location location;
+	private GregorianCalendar start;
+	private GregorianCalendar end;
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public GregorianCalendar getStart() {
+		return start;
+	}
+
+	public void setStart(GregorianCalendar start) {
+		this.start = start;
+	}
+
+	public GregorianCalendar getEnd() {
+		return end;
+	}
+
+	public void setEnd(GregorianCalendar end) {
+		this.end = end;
+	}
+}
---
+++ src/main/java/goa/systems/eventman/EventmgrApplication.java
@@ -0,0 +1,13 @@
+package goa.systems.eventman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmgrApplication.class, args);
+	}
+
+}
